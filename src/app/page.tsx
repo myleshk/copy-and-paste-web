cd7f173f7a5a8a00df134b1450f3afd8bfcd6d81
Myles Fang <git@myles.hk>
2024-07-08 21:46:16 +0800

    feat: add Firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChangeEvent, useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { Frame, Stomp } from "@stomp/stompjs";
+import { CompatClient, Frame, Stomp } from "@stomp/stompjs";
 import MessagesView from "@/components/messagesView";
 import Message from "@/models/message";
 import User from "@/models/user";
@@ -9,8 +9,6 @@ import { generateId } from "@/utils/crypto";
 import UsersView from "@/components/usersView";
 import SockJS from "sockjs-client";
 
-const socket = new SockJS(process.env.REACT_APP_WEBSOCKET_PATH ?? 'http://localhost:8888/ws')
-const stompClient = Stomp.over(socket);
 
 export default function Home() {
   const [userId, setUserId] = useState<string | null>(null);
@@ -18,11 +16,12 @@ export default function Home() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [users, setUsers] = useState<User[]>([]);
   const [selectedUserId, setSelectedUserId] = useState("");
+  const stompClient = useRef<CompatClient | null>(null);
 
   const inputDisabled = useMemo(() => !userId || !selectedUserId, [selectedUserId, userId]);
   const inputButtonDisabled = useMemo(() => inputDisabled || !inputMessage, [inputDisabled, inputMessage]);
-  const otherUsers = useMemo(() => users.filter(u => u.id !== userId), [userId, users])
-  const me = useMemo(() => users.find(u => u.id == userId), [userId, users])
+  const otherUsers = useMemo(() => users.filter(u => u.id !== userId), [userId, users]);
+  const me = useMemo(() => users.find(u => u.id == userId), [userId, users]);
 
   const onMessage = useCallback((message: Message) => {
     setMessages(oldMessages => {
@@ -39,20 +38,6 @@ export default function Home() {
     setSelectedUserId(userId);
   }, []);
 
-  const connect = useCallback(() => {
-
-    stompClient.connect({}, (frame: Frame) => {
-      const userId = frame.headers['user-name'];
-      setUserId(userId);
-      console.log(`Connected with user ID ${userId}`);
-
-      stompClient.subscribe('/topic/user', ({ body }) => {
-        const newUsers: User[] = JSON.parse(body);
-        setUsers(newUsers);
-      });
-    });
-  }, []);
-
   const sendMessage = useCallback(() => {
     const message: Message = {
       id: generateId(),
@@ -61,7 +46,7 @@ export default function Home() {
       createdAt: new Date().toJSON(),
       body: inputMessage
     };
-    stompClient.publish({
+    stompClient.current!.publish({
       destination: `/app/message`,
       body: JSON.stringify(message)
     });
@@ -75,28 +60,38 @@ export default function Home() {
     setInputMessage(e.target.value);
   }, []);
 
-  useEffect(() => {
-    connect();
-  }, [connect]);
+  useEffect(()=>{
+    const socket = new SockJS(process.env.NEXT_PUBLIC_WEBSOCKET_PATH || '/ws')
+    stompClient.current = Stomp.over(socket);
 
-  useEffect(() => {
-    stompClient.onWebSocketError = (error) => {
+    stompClient.current.onWebSocketError = (error) => {
       setUserId("");
       console.error('Error with websocket', error);
     };
 
-    stompClient.onStompError = (frame) => {
+    stompClient.current.onStompError = (frame) => {
       console.error('Broker reported error: ' + frame.headers['message']);
       console.error('Additional details: ' + frame.body);
     };
+    
+    // connect
+    stompClient.current.connect({}, (frame: Frame) => {
+      const userId = frame.headers['user-name'];
+      setUserId(userId);
+      console.log(`Connected with user ID ${userId}`);
 
-  }, [onMessage, users]);
+      stompClient.current!.subscribe('/topic/user', ({ body }) => {
+        const newUsers: User[] = JSON.parse(body);
+        setUsers(newUsers);
+      });
+    });
+  },[]);
 
   useEffect(() => {
     if (!userId) {
       return;
     }
-    const subscription = stompClient.subscribe(`/user/queue/message`, ({ body }) => {
+    const subscription = stompClient.current!.subscribe(`/user/queue/message`, ({ body }) => {
       onMessage(JSON.parse(body));
     });
     return function cleanup() {
@@ -120,9 +115,9 @@ export default function Home() {
         ) : (
           <div>
             <p>
-              Connection error
+              Connection error.
+              Please refresh page to try again.
             </p>
-            <button className="btn btn-blue" onClick={connect}>Retry connect</button>
           </div>
         )) :
           <>
